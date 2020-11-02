e6fceb89bf86f9d78dc3003afc925a75a0a96a54
Lucas Grigolon Varela <lucasgvarela@pm.me>
2020-11-02 13:50:09 -0300

    lint

--- map_iteration/map_iteration.go
+++ map_iteration/map_iteration.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 func main() {
-  m := map[string]int{
-    "one": 1,
-    "two": 2,
-    "three": 3,
-  }
+	m := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+	}
 
-  for k, v := range m {
-    fmt.Println(k, v)
-  }
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 
 }
